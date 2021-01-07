5e5f59c5118d9265251b57c288995650dcda0233
Galatolol <galatolol@protonmail.com>
2021-01-08 00:16:33 +0100

    games

---
+++ src/data/games/index.js
@@ -0,0 +1,722 @@
+let games = {};
+
+games["1817"] = {
+  id: "1817",
+  slug: "1817",
+  file: "1817.json",
+  title: "1817",
+  designer: "Craig Bartell and Tim Flowers",
+  minPlayers: 3,
+  maxPlayers: 7,
+};
+games["1817NA"] = {
+  id: "1817NA",
+  slug: "1817NA",
+  file: "1817NA.json",
+  title: "1817NA",
+  designer: "Marc Voyer",
+  minPlayers: 3,
+  maxPlayers: 7,
+};
+games["1824GoodsTime"] = {
+  id: "1824GoodsTime",
+  slug: "1824GoodsTime",
+  file: "1824GoodsTime.json",
+  title: "1824 Goods Time variant",
+  designer: "Lonny Orgler",
+};
+games["1825-D1"] = {
+  id: "1825-D1",
+  slug: "1825-D1",
+  file: "1825-D1.json",
+  title: "1825 D1",
+  designer: "Francis Tresham",
+};
+games["1825-DB1"] = {
+  id: "1825-DB1",
+  slug: "1825-DB1",
+  file: "1825-DB1.json",
+  title: "1825 DB1",
+  designer: "Francis Tresham",
+};
+games["1825-DB3"] = {
+  id: "1825-DB3",
+  slug: "1825-DB3",
+  file: "1825-DB3.json",
+  title: "1825 DB3",
+  designer: "Francis Tresham",
+};
+games["1825-DB5"] = {
+  id: "1825-DB5",
+  slug: "1825-DB5",
+  file: "1825-DB5.json",
+  title: "1825 DB5",
+  designer: "Francis Tresham",
+};
+games["1825-K1"] = {
+  id: "1825-K1",
+  slug: "1825-K1",
+  file: "1825-K1.json",
+  title: "1825 K1",
+  designer: "Francis Tresham",
+};
+games["1825-K3"] = {
+  id: "1825-K3",
+  slug: "1825-K3",
+  file: "1825-K3.json",
+  title: "1825 K3",
+  designer: "Francis Tresham",
+};
+games["1825-K5"] = {
+  id: "1825-K5",
+  slug: "1825-K5",
+  file: "1825-K5.json",
+  title: "1825 K5",
+  designer: "Francis Tresham",
+};
+games["1825-K6"] = {
+  id: "1825-K6",
+  slug: "1825-K6",
+  file: "1825-K6.json",
+  title: "1825 K6",
+  designer: "Francis Tresham",
+};
+games["1825-Met"] = {
+  id: "1825-Met",
+  slug: "1825-Met",
+  file: "1825-Met.json",
+  title: "1825 - Met Line",
+  designer: "Francis Tresham",
+};
+games["1825-R1.1"] = {
+  id: "1825-R1.1",
+  slug: "1825-R1.1",
+  file: "1825-R1.1.json",
+  title: "1825 R1.1",
+  designer: "Francis Tresham",
+};
+games["1825-R2"] = {
+  id: "1825-R2",
+  slug: "1825-R2",
+  file: "1825-R2.json",
+  title: "1825 R2",
+  designer: "Francis Tresham",
+};
+games["1825-R3.1"] = {
+  id: "1825-R3.1",
+  slug: "1825-R3.1",
+  file: "1825-R3.1.json",
+  title: "1825 R3.1",
+  designer: "Francis Tresham",
+};
+games["1825-U1"] = {
+  id: "1825-U1",
+  slug: "1825-U1",
+  file: "1825-U1.json",
+  title: "1825 Unit 1",
+  designer: "Francis Tresham",
+};
+games["1825-U2"] = {
+  id: "1825-U2",
+  slug: "1825-U2",
+  file: "1825-U2.json",
+  title: "1825 Unit 2",
+  designer: "Francis Tresham",
+};
+games["1825-U3"] = {
+  id: "1825-U3",
+  slug: "1825-U3",
+  file: "1825-U3.json",
+  title: "1825 Unit 3",
+  designer: "Francis Tresham",
+};
+games["1825"] = {
+  id: "1825",
+  slug: "1825",
+  file: "1825.json",
+  title: "1825",
+  designer: "Francis Tresham",
+  minPlayers: 4,
+  maxPlayers: 9,
+};
+games["1830PL"] = {
+  id: "1830PL",
+  slug: "1830PL",
+  file: "1830PL.json",
+  title: "1830PL",
+  subtitle: "Historically inaccurate railroading in Poland",
+  designer: "",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1832"] = {
+  id: "1832",
+  slug: "1832",
+  file: "1832.json",
+  title: "1832",
+  subtitle: "The South",
+  designer: "W.R. Dixon",
+  minPlayers: 2,
+  maxPlayers: 7,
+};
+games["1834"] = {
+  id: "1834",
+  slug: "1834",
+  file: "1834.json",
+  title: "1834",
+  subtitle: "Belgium & Luxembourg",
+  designer: "Eddie Robbins",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1836"] = {
+  id: "1836",
+  slug: "1836",
+  file: "1836.json",
+  title: "1836",
+  designer: "Eddie Robbins",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1838TL"] = {
+  id: "1838TL",
+  slug: "1838TL",
+  file: "1838TL.json",
+  title: "1838TL",
+  subtitle: "The Thuringia Connection",
+  designer: "Dieter Danziger",
+  minPlayers: 3,
+  maxPlayers: 8,
+};
+games["1839NL"] = {
+  id: "1839NL",
+  slug: "1839NL",
+  file: "1839NL.json",
+  title: "1839",
+  designer: "Paul Stouthard, Rob van Wijngaarden",
+};
+games["1841"] = {
+  id: "1841",
+  slug: "1841",
+  file: "1841.json",
+  title: "1841",
+  subtitle: "Railways in Northern Italy",
+  designer: "Federico Vellani and Manlio Manzini",
+  minPlayers: 3,
+  maxPlayers: 8,
+};
+games["1841lite"] = {
+  id: "1841lite",
+  slug: "1841lite",
+  file: "1841lite.json",
+  title: "1841 Lite",
+  designer: "Federico Vellani and Manlio Manzini",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1844"] = {
+  id: "1844",
+  slug: "1844",
+  file: "1844.json",
+  title: "1844",
+  subtitle: "Switzerland",
+  designer: "Helmut Ohley and Peter Minder",
+  minPlayers: 3,
+  maxPlayers: 7,
+};
+games["1845"] = {
+  id: "1845",
+  slug: "1845",
+  file: "1845.json",
+  title: "1845",
+  designer: "Kevin Dykman",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1846"] = {
+  id: "1846",
+  slug: "1846",
+  file: "1846.json",
+  title: "1846",
+  subtitle: "The Race for the Midwest",
+  designer: "Thomas Lehmann",
+  minPlayers: 2,
+  maxPlayers: 5,
+};
+games["1848"] = {
+  id: "1848",
+  slug: "1848",
+  file: "1848.json",
+  title: "1848",
+  subtitle: "Australia",
+  designer: "Helmut Ohley & Leonhard Orgler",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1849"] = {
+  id: "1849",
+  slug: "1849",
+  file: "1849.json",
+  title: "1849",
+  subtitle: "The Game of Sicilian Railways",
+  designer: "Federico Vellani",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1854"] = {
+  id: "1854",
+  slug: "1854",
+  file: "1854.json",
+  title: "1854",
+  subtitle: "Austria",
+  designer: "Lonny Orgler",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1856"] = {
+  id: "1856",
+  slug: "1856",
+  file: "1856.json",
+  title: "1856",
+  subtitle: "Railroading in Upper Canada from 1856",
+  designer: "Bill Dixon",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1857"] = {
+  id: "1857",
+  slug: "1857",
+  file: "1857.json",
+  title: "1857",
+  designer: "Eddie Robbins",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1858"] = {
+  id: "1858",
+  slug: "1858",
+  file: "1858.json",
+  title: "1858",
+  subtitle: "The Railways of Iberia",
+  designer: "Ian Wilson",
+  publisher: "aag",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1859"] = {
+  id: "1859",
+  slug: "1859",
+  file: "1859.json",
+  title: "1859",
+  subtitle: "South Africa",
+  designer: "David V. H. Peters",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1860"] = {
+  id: "1860",
+  slug: "1860",
+  file: "1860.json",
+  title: "1860",
+  subtitle: "Railways on the Isle of Wight",
+  designer: "Mike Hutton",
+  minPlayers: 2,
+  maxPlayers: 4,
+};
+games["1867"] = {
+  id: "1867",
+  slug: "1867",
+  file: "1867.json",
+  title: "1867",
+  subtitle: "The Railways of Canada",
+  designer: "Ian Wilson",
+  publisher: "gtg",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1868"] = {
+  id: "1868",
+  slug: "1868",
+  file: "1868.json",
+  title: "1868",
+  subtitle: "Uruguay",
+  designer: "Eddie Robbins",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1870"] = {
+  id: "1870",
+  slug: "1870",
+  file: "1870.json",
+  title: "1870",
+  subtitle: "Railroading across the Trans Mississippi from 1870",
+  designer: "Bill Dixon",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["1871BC"] = {
+  id: "1871BC",
+  slug: "1871BC",
+  file: "1871BC.json",
+  title: "1871: British Columbia",
+  subtitle: "Confederation Railways",
+  designer: "Brennan Sheremeto",
+  publisher: "self",
+  minPlayers: 2,
+  maxPlayers: 4,
+};
+games["1879"] = {
+  id: "1879",
+  slug: "1879",
+  file: "1879.json",
+  title: "1879",
+  subtitle: "US Pacific Northwest",
+  designer: "Eddie Robbins",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["1880"] = {
+  id: "1880",
+  slug: "1880",
+  file: "1880.json",
+  title: "1880",
+  subtitle: "China",
+  designer: "Helmut Ohley & Leonhard Orgler",
+  minPlayers: 3,
+  maxPlayers: 7,
+};
+games["1882"] = {
+  id: "1882",
+  slug: "1882",
+  file: "1882.json",
+  title: "1882",
+  designer: "Marc Voyer",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["1883ExpressdOrient"] = {
+  id: "1883ExpressdOrient",
+  slug: "1883ExpressdOrient",
+  file: "1883ExpressdOrient.json",
+  title: "1883 Express d'Orient",
+  designer: "Anthony Fryer",
+};
+games["1888"] = {
+  id: "1888",
+  slug: "1888",
+  file: "1888.json",
+  title: "1888",
+  designer: "Anthony Fryer",
+};
+games["1889"] = {
+  id: "1889",
+  slug: "1889",
+  file: "1889.json",
+  title: "1889",
+  subtitle: "History of Shikoku Railways",
+  designer: "Yasutaka Ikeda",
+  publisher: "gtg",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["1891"] = {
+  id: "1891",
+  slug: "1891",
+  file: "1891.json",
+  title: "1891",
+  subtitle: "Railroading in Hiroshima",
+  designer: "Toryo Hojo",
+};
+games["1894"] = {
+  id: "1894",
+  slug: "1894",
+  file: "1894.json",
+  title: "1894",
+  subtitle: "Rise and fall of railway companies (hopefully)",
+  designer: "",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["1899"] = {
+  id: "1899",
+  slug: "1899",
+  file: "1899.json",
+  title: "1899",
+  designer: "Dirk Clemens & Ingo Mayer",
+};
+games["18Brass"] = {
+  id: "18Brass",
+  slug: "18Brass",
+  file: "18Brass.json",
+  title: "18Brass",
+  subtitle: "VERY EARLY PROTOTYPE",
+  designer: "Galatolol",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18C2C"] = {
+  id: "18C2C",
+  slug: "18C2C",
+  file: "18C2C.json",
+  title: "18C2C",
+  subtitle: "Manifest Destiny",
+  designer: "Mark Frazier",
+  minPlayers: 2,
+  maxPlayers: 8,
+};
+games["18Chesapeake"] = {
+  id: "18Chesapeake",
+  slug: "18Chesapeake",
+  file: "18Chesapeake.json",
+  title: "18Chesapeake",
+  subtitle: "The Birth of the American Railroads",
+  designer: "Scott Petersen",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["18CLE"] = {
+  id: "18CLE",
+  slug: "18CLE",
+  file: "18CLE.json",
+  title: "18CLE",
+  subtitle: "Trolly Car Compaines of Cleveland, Ohio",
+  designer: " Richard McGuire",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["18EB"] = {
+  id: "18EB",
+  slug: "18EB",
+  file: "18EB.json",
+  title: "18EB",
+  designer: "Michael Carter, Jeremy Long and Alexandra Sime",
+  publisher: "self",
+  minPlayers: 2,
+  maxPlayers: 4,
+};
+games["18EU"] = {
+  id: "18EU",
+  slug: "18EU",
+  file: "18EU.json",
+  title: "18EU",
+  subtitle: "Railraoding in Europe from the 1830's to the 1930's",
+  designer: "David G. D. Hecht",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["18GJ"] = {
+  id: "18GJ",
+  slug: "18GJ",
+  file: "18GJ.json",
+  title: "18 Grand Junction",
+  designer: "Anthony Fryer",
+};
+games["18H8U"] = {
+  id: "18H8U",
+  slug: "18H8U",
+  file: "18H8U.json",
+  title: "18H8U",
+  designer: "Mark Derrick",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18IrePL"] = {
+  id: "18IrePL",
+  slug: "18IrePL",
+  file: "18IrePL.json",
+  title: "18IrePL",
+  designer: "Galatolol",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18Kaas"] = {
+  id: "18Kaas",
+  slug: "18Kaas",
+  file: "18Kaas.json",
+  title: "18Kaas",
+  designer: "Erno Eekelschot",
+  publisher: "ERJO Games",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["18LA"] = {
+  id: "18LA",
+  slug: "18LA",
+  file: "18LA.json",
+  title: "18 Los Angeles",
+  subtitle: "Railroading in the City of Angels",
+  designer: "Tony Fryer",
+  minPlayers: 2,
+  maxPlayers: 4,
+};
+games["18Mex"] = {
+  id: "18Mex",
+  slug: "18Mex",
+  file: "18Mex.json",
+  title: "18Mex",
+  designer: "Mark Derrick",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18MI"] = {
+  id: "18MI",
+  slug: "18MI",
+  file: "18MI.json",
+  title: "18MI",
+  designer: "Robert G. Schroeder",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["18Misc"] = {
+  id: "18Misc",
+  slug: "18Misc",
+  file: "18Misc.json",
+  title: "18Misc",
+  designer: "",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18MM"] = {
+  id: "18MM",
+  slug: "18MM",
+  file: "18MM.json",
+  title: "18MM",
+  designer: "Mark Hendrickson",
+};
+games["18NC"] = {
+  id: "18NC",
+  slug: "18NC",
+  file: "18NC.json",
+  title: "18NC",
+  subtitle: "Railroads of the Tar Heel State",
+  designer: "Matt Wilson",
+  publisher: "self",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18NK"] = {
+  id: "18NK",
+  slug: "18NK",
+  file: "18NK.json",
+  title: "18NK",
+  subtitle: "Nagaokakyo City",
+  designer: "CTDA",
+};
+games["18Scan"] = {
+  id: "18Scan",
+  slug: "18Scan",
+  file: "18Scan.json",
+  title: "18Scan",
+  subtitle: "Railroading under the Northern Lights",
+  designer: "David G. D. Hecht",
+  minPlayers: 2,
+  maxPlayers: 4,
+};
+games["18SJ"] = {
+  id: "18SJ",
+  slug: "18SJ",
+  file: "18SJ.json",
+  title: "18SJ",
+  subtitle: "Let there be rail",
+  designer: "Örjan Wennman",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["18SS"] = {
+  id: "18SS",
+  slug: "18SS",
+  file: "18SS.json",
+  title: "18SS",
+  designer: "CTDA",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["18SY"] = {
+  id: "18SY",
+  slug: "18SY",
+  file: "18SY.json",
+  title: "18SY",
+  subtitle: "Railroading in Southern Yamaguchi",
+  designer: "Toryo Hojo",
+};
+games["18TE"] = {
+  id: "18TE",
+  slug: "18TE",
+  file: "18TE.json",
+  title: "18TE",
+  subtitle: "Eisenbahnen im Tecklenburger Land",
+  designer: "Herbert Harengel",
+  publisher: "self",
+  minPlayers: 2,
+  maxPlayers: 3,
+};
+games["18Test"] = {
+  id: "18Test",
+  slug: "18Test",
+  file: "18Test.json",
+  title: "18Test",
+  subtitle: "18xx-Maker Test File",
+  designer: "Christopher Giroir",
+  minPlayers: 1,
+  maxPlayers: 6,
+};
+games["18TK"] = {
+  id: "18TK",
+  slug: "18TK",
+  file: "18TK.json",
+  title: "18TK",
+  subtitle: "Railroading in the Tokyo Metropolis",
+  designer: "Julian Okuda",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["18TN"] = {
+  id: "18TN",
+  slug: "18TN",
+  file: "18TN.json",
+  title: "18TN",
+  subtitle: "The Railroads Comes to Tennessee",
+  designer: "Mark Derrick",
+  minPlayers: 3,
+  maxPlayers: 5,
+};
+games["18TraXX2020"] = {
+  id: "18TraXX2020",
+  slug: "18TraXX2020",
+  file: "18TraXX2020.json",
+  title: "18TraXX",
+  subtitle: "2020: Great Plains Railroading",
+  designer: "Michael Carter and Anthony Fryer",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+games["18USA"] = {
+  id: "18USA",
+  slug: "18USA",
+  file: "18USA.json",
+  title: "18USA",
+  designer: "Shawn Fox, Mark Hendrickson and Edward Reece",
+  minPlayers: 3,
+  maxPlayers: 7,
+};
+games["21Moon"] = {
+  id: "21Moon",
+  slug: "21Moon",
+  file: "21Moon.json",
+  title: "21Moon",
+  designer: "Jonas Jones",
+  minPlayers: 3,
+  maxPlayers: 6,
+};
+games["FourthAge"] = {
+  id: "FourthAge",
+  slug: "FourthAge",
+  file: "FourthAge.json",
+  title: "Fourth Age",
+  subtitle: "Railroading in Middle Earth",
+  designer: "Rick Dutton",
+  minPlayers: 2,
+  maxPlayers: 6,
+};
+
+export default games;
